fbfe665862029cda5906093e87e2ffba38de34aa
gavinlen <gavin_len@163.com>
2020-06-09 23:07:12 +0800

    add README.md File

---
+++ README.md
@@ -0,0 +1,9 @@
+# 1. 服务注册中心
+## 1.1 Eureka
+
+## 1.2 Zookeeper
+
+## 1.3 Consul
+
+# 2. 负载均衡
+## 2.1 Ribbon
---
+++ cloud-gateway/src/main/java/xyz/ieden/learn/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package xyz.ieden.learn.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GatewayApplication
+ *
+ * @author gavin
+ * @version 1.0
+ * @datetime 2020/6/9 21:58
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
